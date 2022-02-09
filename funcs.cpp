5fc8ff912ea145cf674189054a268df7af1fe322
mahathirrojan <torbengalifreind@gmail.com>
2022-02-09 00:46:49 -0500

    added task B files (unsure on output dude to '%')

---
+++ funcs.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+#include "funcs.h" 
+
+void print_interval(int L, int U){
+    int i; 
+    for(i = L; i < U; i++){
+        std::cout << " " << i << " ";
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+#include "funcs.h"
+
+
+int main(){
+    print_interval(1,5);
+    return 0; 
+}
